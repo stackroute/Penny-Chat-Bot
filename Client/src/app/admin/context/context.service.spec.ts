7abf1cb37793e8b42ef729e52a37d86207fd5f1f
traning <agarwal.ankur0709@gmail.com>
2017-11-08 14:20:32 +0530

    Ankur pull and push

--- Client/src/app/admin/context/context.service.spec.ts
+++ Client/src/app/admin/context/context.service.spec.ts
@@ -1,15 +1,85 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { ContextService } from './context.service';
-
+import { TestBed, inject, async, getTestBed ,tick, fakeAsync } from '@angular/core/testing';
+import { Http, HttpModule , XHRBackend, ConnectionBackend,Connection, BaseRequestOptions, Response, ResponseOptions,RequestMethod} from '@angular/http';
+import {ContextService } from './context.service';
+import {MockBackend, MockConnection } from '@angular/http/testing';
+import {testConfig} from './context.config';
 describe('ContextService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ContextService]
-    });
-  });
+    let mockBackend:MockBackend;
+    let contextService:ContextService;
+ beforeEach(() => {
+   TestBed.configureTestingModule({
+     providers: [ContextService,
+                MockBackend,
+                BaseRequestOptions,
+                {
+                    provide: Http,
+                    deps: [MockBackend, BaseRequestOptions],
+                    useFactory:
+                    (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+                        return new Http(backend, defaultOptions);
+                    }
+                }]
+   }).compileComponents();
+   contextService =getTestBed().get(ContextService);
+   mockBackend = TestBed.get(MockBackend);
+ });
 
-  it('should be created', inject([ContextService], (service: ContextService) => {
-    expect(service).toBeTruthy();
-  }));
+ it('should be created', inject([ContextService], (service: ContextService) => {
+   expect(service).toBeTruthy();
+ }));
+
+
+
+
+//---------------------------Positive testcase for getallcontext----------------------------------
+it('should check getAllContext() positive case', fakeAsync(() => {
+
+     let mockResponse=testConfig.response
+
+      mockBackend.connections.subscribe((connection:MockConnection)=>{
+         expect(connection.request.method).toBe(RequestMethod.Get);
+         tick();
+         connection.mockRespond(new Response(new ResponseOptions({
+             body:mockResponse
+         })));
+     });
+
+     let data=testConfig.responses
+        
+
+   
+     contextService.getAllContext().subscribe((res)=>{
+         expect(res).toBeDefined();
+         console.log('aggagaggaggagaga',res[0]._fields[0].properties.name);
+         console.log('smsmmmsmmsms',data._fields[0].properties.name)
+         expect(res[0]._fields[0].properties.name).toEqual(data._fields[0].properties.name);
+     });
+ }));
+
+
+//---------------------------negative testcase for getAllcontext----------------------------------
+it('should check getAllContext() negative case', fakeAsync(() => {
+
+     let mockResponse=testConfig.response
+    
+
+      mockBackend.connections.subscribe((connection:MockConnection)=>{
+         expect(connection.request.method).toBe(RequestMethod.Get);
+         tick();
+         connection.mockRespond(new Response(new ResponseOptions({
+             body:mockResponse
+         })));
+     });
+
+     let data=testConfig.responses
+   
+     contextService.getAllContext().subscribe((res)=>{
+         expect(res).toBeDefined();
+         expect(res[0]._fields[0].properties.name).not.toEqual(null);
+     });
+ }));
+
+
+
+
 });
