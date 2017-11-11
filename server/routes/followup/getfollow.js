7f9c82b1639241c1c2950dbbd25f9a46b47430f4
Naman11 <Naman11>
2017-11-11 16:53:50 +0530

    Pull

--- server/routes/followup/getfollow.js
+++ server/routes/followup/getfollow.js
@@ -5,7 +5,7 @@ import staticConfig from './Config';
 export default (req,res)=>{
 	try{
 			//add_task.insertOne({taskname : req.body.message[0].TaskName , data : req.body.message },(error,data)=>{
-				flow_schema.find((error,data)=>{q
+				flow_schema.find((error,data)=>{
 					if(data.length > 0){
 			logger.info(staticConfig.getFollow.messageFound)		//making logs
 			res.json(data);
