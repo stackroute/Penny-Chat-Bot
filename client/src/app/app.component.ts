1a170b5040b04cd80d3eef53a8e821b155831c1f
Mohi29 <info.mohitg@gmail.com>
2017-11-13 11:27:53 +0530

    mohit fb bug done

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+//facebook redirect
+if (window.location.hash == '#_=_'){
+    history.replaceState 
+        ? history.replaceState(null, null, window.location.href.split('#')[0])
+        : window.location.hash = '';
+}
+
 @Component({
   // tslint:disable-next-line
   selector: 'body',
