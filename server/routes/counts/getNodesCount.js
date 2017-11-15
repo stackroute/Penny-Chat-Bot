7900e3df834b8d8a5ed083db9cce336cfd8d5c03
Abhishek Gupta <abhishek.gupta_cs13@gla.ac.in>
2017-11-15 11:01:15 +0530

    Update getNodesCount.js

--- server/routes/counts/getNodesCount.js
+++ server/routes/counts/getNodesCount.js
@@ -1,7 +1,7 @@
 let express = require('express');
 const neo4j = require('neo4j-driver').v1;
 import config from '../../config/config';
-/*import staticConfig from './Config';*/
+import staticConfig from './Config';
 import logger from '../../log4js';
 const driver = neo4j.driver(config.neo4jUrl, neo4j.auth.basic("neo4j", config.neo4jurlpassword));
 
@@ -9,32 +9,36 @@ const session = driver.session();
 
 export default (req, res)=> {
 	try{
-		console.log("You are here ");
 		let intent = [];
 		let resultPromise = session.run(
 			'match(domain:Domain) return count(domain)'
 			);
 		resultPromise.then((result)=>{
 			console.log(result.records[0]._fields[0].low)
-			intent.push({"Domain" : result.records[0]._fields[0].low})
+			intent.push({[staticConfig.Domain] : result.records[0]._fields[0].low})
 			const resultPromise = session.run(
 				'match(subdomain:SubDomain) return count(subdomain)'
 				);
 			resultPromise.then((result)=>{
-				intent.push({"SubDomain" : result.records[0]._fields[0].low})
+				intent.push({[staticConfig.SubDomain] : result.records[0]._fields[0].low})
 
 				const resultPromise = session.run(
 					'match(entity:Entity) return count(entity)'
 					);
 				resultPromise.then((result)=>{
-					intent.push({"Entity" : result.records[0]._fields[0].low})
-					console.log(intent.length, "  " , intent);
-					res.json(intent);
+					intent.push({[staticConfig.Entity] : result.records[0]._fields[0].low})
+					const resultPromise = session.run(
+						'match(intent:Intent) return count(intent)' 
+						)
+					resultPromise.then((result) => {
+						intent.push({[staticConfig.Intent] : result.records[0]._fields[0].low})
+						res.json(intent);
+					})
 				})
 			})
 		})
-}catch(error){                                            // error handle if suddenly error occur in database
-   logger.info(staticConfig.editAddSynonym.Error);                  // making logs
-   res.json({status:false, message:staticConfig.editAddSynonym.Error,data:error});
+}catch(error){           // error handle if suddenly error occur in database
+   logger.info(staticConfig.errorMessage);        // making logs
+   res.json({status:false, message:staticConfig.errorMessage,data:error});
  }
 };
